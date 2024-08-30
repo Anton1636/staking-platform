bafaa135400dcb62a540a6c82d82dea696e86331
Hobermac <54035686+Anton1636@users.noreply.github.com>
2024-08-30 16:52:06 +0300

    created header and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,90 @@
 import '@/styles/globals.css'
+import toast, { Toaster } from 'react-hot-toast'
+import merge from 'lodash.merge'
+import '@rainbow-me/rainbowkit/styles.css'
+import {
+	getDefaultWallets,
+	RainbowKitProvider,
+	darkTheme,
+	midnightTheme,
+} from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+
+const HOLESKY = process.env.NEXT_PUBLIC_HOLESKY_RPC_URL
+const EXPLORER = process.env.NEXT_PUBLIC_EXPLORER
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
+const CURRENCY = process.env.NEXT_PUBLIC_CURRENCY
+const DECIMALS = process.env.NEXT_PUBLIC_NETWORK_DECIMALS
+const NAME = process.env.NEXT_PUBLIC_NETWORK_NAME
+const NETWORK = process.env.NEXT_PUBLIC_NETWORK
 
 export default function App({ Component, pageProps }) {
+	const { chains, provider } = configureChains(
+		[
+			{
+				id: Number(CHAIN_ID),
+				name: NAME,
+				network: NETWORK,
+				nativeCurrency: {
+					name: NAME,
+					symbol: CURRENCY,
+					decimals: Number(DECIMALS),
+				},
+				rpcUrls: {
+					default: {
+						http: [`${HOLESKY}`],
+					},
+					public: {
+						http: [`${HOLESKY}`],
+					},
+				},
+				blockExplorers: {
+					default: {
+						name: 'Holescan',
+						url: EXPLORER,
+					},
+				},
+				testnets: true, // while using mainnet need to be false
+			},
+		],
+		[
+			jsonRpcProvider({
+				rpc: chain => {
+					if (chain.id === Number(CHAIN_ID)) {
+						return { http: `${HOLESKY}` }
+					}
+					return null
+				},
+				priority: 1,
+			}),
+		]
+	)
+	const { connectors } = getDefaultWallets({
+		appName: 'StakingPlatform',
+		chains,
+	})
+	const wagmiClient = createClient({
+		autoConnect: true,
+		connectors,
+		provider,
+	})
+	const myTheme = merge(midnightTheme(), {
+		colors: {
+			accentColor: '#562C7B',
+			accentColorForeground: '#FFFFFF',
+		},
+	})
+
 	return (
 		<>
-			<Component {...pageProps} />
+			<WagmiConfig client={wagmiClient}>
+				<RainbowKitProvider chains={chains} theme={myTheme}>
+					<Component {...pageProps} />
+					<Toaster />
+				</RainbowKitProvider>
+			</WagmiConfig>
+
 			<script src='js/bootstrap.bundle.min.js'></script>
 			<script src='js/smooth-scrollbar.js'></script>
 			<script src='js/splide.min.js'></script>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useAccount } from 'wagmi'
+import {
+	Header,
+	HeroSection,
+	Footer,
+	Pools,
+	PoolsModel,
+	WithdrawModal,
+	Withdraw,
+	Partners,
+	Statistics,
+	Token,
+	Loader,
+	Notification,
+	ICOSale,
+	Contact,
+	Ask,
+} from '../components/index'
+import {
+	CONTRACT_DATA,
+	deposit,
+	withdraw,
+	claimReward,
+	addTokenToMetaMask,
+} from '../context/index'
 
 const index = () => {
-	return <div>index</div>
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	)
 }
 
 export default index
